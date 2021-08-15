2debea943a102c6ac76c481285935b30312d8c74
emmmwangxiaowang <954544828@qq.com>
2021-08-15 14:52:14 +0800

    日志

---
+++ mybatis-04/src/main/java/com/wang/utils/MybatisUtils.java
@@ -0,0 +1,43 @@
+package com.wang.utils;
+
+/**
+ * @Author: 王航
+ * @Email: 954544828@qq.com
+ * @Date: 2021/8/14 0014
+ */
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+
+//sqlSessionFactory --->sqlSession
+public class MybatisUtils
+{
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static
+    {
+
+        try
+        {
+            //使用mybatis第一步： 获取sqlSessionFactory对象
+            String      resource    = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //既然有了SqlSessionFactory，顾名思义，我们可以从中获得 SqlSession 的实例。org.apache.ibatis.session.SqlSession 提供了在数据库执行 SQL 命令所需的所有方法。
+    // 你可以通过 SqlSession 实例来直接执行已映射的 SQL 语句。例如：
+    public static SqlSession getSqlSession()
+    {
+        return sqlSessionFactory.openSession();
+    }
+
+}
+
--- mybatis-04/src/test/java/dao/userMapperTest.java
+++ mybatis-04/src/test/java/dao/userMapperTest.java
@@ -4,26 +4,37 @@ import com.wang.dao.userMapper;
 import com.wang.pojo.User;
 import com.wang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class userMapperTest
 {
 
+    static Logger logger = Logger.getLogger(userMapper.class);
+
+
     @Test
-    public void test()
+    public void getUserById()
     {
         //1.获得sqlsession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         //方式一:2.执行sql
-        userMapper  mapper   = sqlSession.getMapper(userMapper.class);
+        userMapper  mapper   = sqlSession.  getMapper(userMapper.class);
         User user = mapper.getUserById(2);
-
+        logger.debug("debug:进入了getUserById");
         System.out.println(user);
 
         sqlSession.commit();
         //关闭sqlSession
         sqlSession.close();
+    }
+
+    @Test
+    public void testLog4j(){
+        logger.info("info:进入了testLog4j");
+        logger.debug("debug:进入了testLog4j");
+        logger.error("error:进入了testL0g4j");
     }
 
 }
