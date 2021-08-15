d0ad1c31a8d3da41a78b3a45206317ed681e8601
emmmwangxiaowang <954544828@qq.com>
2021-08-15 17:22:31 +0800

    分页

--- mybatis-04/src/main/java/com/wang/dao/userMapper.java
+++ mybatis-04/src/main/java/com/wang/dao/userMapper.java
@@ -1,15 +1,18 @@
 package com.wang.dao;
 
 import com.wang.pojo.User;
+import java.util.List;
+import java.util.Map;
 
 public interface userMapper
 {
 
 
 
+    //分页
+    List<User> getUserByLimit(Map<String,Integer> map);
+
     //根据id查询用户
     User getUserById(int id);
-
-
 
 }
--- mybatis-04/src/test/java/dao/userMapperTest.java
+++ mybatis-04/src/test/java/dao/userMapperTest.java
@@ -3,9 +3,13 @@ package dao;
 import com.wang.dao.userMapper;
 import com.wang.pojo.User;
 import com.wang.utils.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class userMapperTest
 {
@@ -35,6 +39,42 @@ public class userMapperTest
         logger.info("info:进入了testLog4j");
         logger.debug("debug:进入了testLog4j");
         logger.error("error:进入了testL0g4j");
+    }
+
+    @Test
+    public void getUserByLimit(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        userMapper           mapper = sqlSession.getMapper(userMapper.class);
+        Map <String, Integer> map    = new HashMap <>();
+        map.put("startIndex",1);
+        map.put("pageSize",1);
+        List <User> userByLimit = mapper.getUserByLimit(map);
+        for (User user : userByLimit)
+        {
+            System.out.println(user);
+        }
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserByRowBounds(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //RowBounds实现
+        RowBounds rowBounds = new RowBounds(1, 2);
+
+        //通过Java代码层面实现分页
+        List <User> userList = sqlSession.selectList("com.wang.dao.userMapper.getUserByRowBounds",null,rowBounds);
+
+        for (User user : userList)
+        {
+            System.out.println(user);
+        }
+
+        sqlSession.commit();
+        sqlSession.close();
     }
 
 }
